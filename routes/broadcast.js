4f329d81f8a8d3ef517f97c7a7a5452a5267e548
Narat Suchartsunthorn <me@mixth.net>
2018-03-27 17:06:54 +0700

    Add example of using broadcast api by sending text message

--- routes/broadcast.js
+++ routes/broadcast.js
@@ -1,4 +1,37 @@
-var express = require('express')
-var router = express.Router()
+const express = require('express')
+const request = require('request-promise-native')
+const router = express.Router()
+
+const { BROADCAST_API_TOKEN } = require('../config.js')
+
+router.get('/send', async (req, res) => {
+    const userId = req.query.userId
+
+    if (!userId || userId === '') {
+        res.sendStatus(400)
+        return
+    }
+
+    let broadcastBody = {
+        "to": [
+            userId
+        ],
+        "msg_type": "text",
+        "msg": "This is a broadcast message",
+        "buttons": [],
+        "quick_reply" : []
+     }
+
+    try {
+        await request
+            .post(`https://hbotconnect.unicornonzen.com/api/sendmessage?accessToken=${BROADCAST_API_TOKEN}`)
+            .json(broadcastBody)
+        res.sendStatus(200)
+    } catch (e) {
+        console.error(e)
+        res.sendStatus(500)
+    }
+})
+
 
 module.exports = router
