0b9ea8d74bf9c0b712fa2de7b9362002a2333429
Prawin Rungpiriyadej <parwin.r@hbot.io>
2018-03-29 16:14:02 +0700

    Handle webhook demo

--- routes/webhook.js
+++ routes/webhook.js
@@ -1,4 +1,37 @@
-var express = require('express')
-var router = express.Router()
+const express = require('express')
+const bodyParser = require('body-parser')
+const config = require('../config')
+
+const router = express.Router()
+
+// middleware : parsing application/json
+router.use(bodyParser.json())
+
+// middleware : parsing application/x-www-form-urlencoded ( body-value: [string, array] )
+router.use(bodyParser.urlencoded({ extended: false }))
+
+// middleware : log every incoming request
+router.use((req, resp, next) => {
+    if (process.env.NODE_ENV !== "production") {
+        console.log(JSON.stringify({
+            query: req.query,
+            headers: req.headers,
+            method: req.method,
+            body: req.body
+        }, null, 2))
+    }
+    next()
+})
+
+// webhook verification 
+router.get('/', (req, resp) => {
+    console.log(`send broadcast_api_token : ${config.BROADCAST_API_TOKEN}`)
+    resp.send(config.BROADCAST_API_TOKEN)
+})
+
+// webhook incoming request
+router.post('/', (req, resp) => {
+    // do something
+})
 
 module.exports = router
