404c2ef5d94429370fe47cad276568f1f2279278
mnosuk <inotecpe25@gmail.com>
2018-05-10 17:47:13 +0700

    no message

--- routes/webhook.js
+++ routes/webhook.js
@@ -6,12 +6,12 @@ const router = express.Router()
 // middleware : log every incoming request
 router.use((req, resp, next) => {
     if (process.env.NODE_ENV !== 'production') {
-        console.log(JSON.stringify({
-            query: req.query,
-            headers: req.headers,
-            method: req.method,
-            body: req.body
-        }, null, 2))
+        // console.log(JSON.stringify({
+        //     query: req.query,
+        //     headers: req.headers,
+        //     method: req.method,
+        //     body: req.body
+        // }, null, 2))
     }
     next()
 })
@@ -29,21 +29,23 @@ router.post('/', (req, resp) => {
             .setAsTypeAgent() // sets the message as type user
             .setPlatform('fs_production') // sets the platform to the given value
             .setMessage(req.body.msg) // the message sent by either user or agent
+            .setAsHandled()
             .setVersion('1.0') // the version that the deployed bot is
             .setUserId(req.body.user_attr.user_id) // a unique string identifying the user which the bot is interacting with
             .send()
-            .then(msg => console.log("BOT SAID:", msg.getCreateResponse()))
+            .then(msg => console.log(`BOT SAID: ${req.body.msg}  ${msg.getCreateResponse()}`))
             .catch(err => console.error("BOT ERR:",err))
     }
-    if(req.body.sender !== process.env.BOT_NAME && req.body.msg_type === 'text') {
+    if(req.body.sender !== process.env.BOT_NAME) {
         const msg = chatbase.newMessage(process.env.CHATBASE_KEY)
 	    .setAsTypeUser() // sets the message as type user
 	    .setPlatform('fs_production') // sets the platform to the given value
-	    .setMessage(req.body.msg) // the message sent by either user or agent
+        .setMessage(req.body.msg) // the message sent by either user or agent
+        .setAsHandled()
 	    .setVersion('1.0') // the version that the deployed bot is
         .setUserId(req.body.user_attr.user_id) // a unique string identifying the user which the bot is interacting with
         .send()
-	    .then(msg => console.log("USER SAID:",msg.getCreateResponse()))
+	    .then(msg => console.log(`USER SAID: ${req.body.msg}  ${msg.getCreateResponse()}`))
         .catch(err => console.error("USER ERR:", err))
     }
 })
